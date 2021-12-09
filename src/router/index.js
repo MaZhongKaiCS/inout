5914d51ca04e3661478ade9e61b31c48f48231bd
mario <mario.ma@usmarthk.com>
2021-12-09 13:48:40 +0800

    解决引入失败问题

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'firstPage',
+    component: () => import(/* webpackChunkName: "about" */ '../views/first-page.vue')
   },
   {
     path: '/in',
@@ -23,7 +22,7 @@ const routes = [
 ]
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   routes
 })
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+// vue.config.js
+module.exports = {
+    // 解决引入失败的问题
+    publicPath : './',
+    outputDir: 'docs'
+}
